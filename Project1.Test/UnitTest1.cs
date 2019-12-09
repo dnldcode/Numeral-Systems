bb20364210360853226e16f990ed1da1442fc68a
Donald <dnldcode@gmail.com>
2019-12-09 01:52:17 +0100

    first commit

---
+++ Project1.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Project1.Test
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Assert.AreEqual(1, 1);
+		}
+	}
+}
